1cf6a1e8ddce54fd00290e6bfc44de4820326c93
digolstudiocode <dickydigol17@gmail.com>
2023-07-06 10:19:03 +0700

    penambahan component About Contact Skills Work

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,17 +6,24 @@ const Home = () => {
     <div name="home" className="bg-[#0a192f] w-full h-screen">
       {/* Container */}
       <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
-        <p>Hi , my name is</p>
-        <h1>Dicky Kurniawan</h1>
-        <h2>I'm a Full Stack Developer.</h2>
-        <p>
+        <p className="text-pink-600">Hi , my name is</p>
+        <h1 className="text-4xl sm:text-7xl font-bold text-[#ccd6f6]">
+          Dicky Kurniawan
+        </h1>
+        <h2 className="text-4xl sm:text-7xl font-bold text-[#8892b0]">
+          I'm a Full Stack Developer.
+        </h2>
+        <p className="text-[#8892b0] py-4 max-w-[700px]">
           I'm a full-stack developer specializing in building (and ossasionally
           designing) exceptional digital experiences. Currently , I'm focused on
           building responsive full-stack web appliactions
         </p>
         <div>
-          <button>
-            View Work <HiArrowNarrowRight />
+          <button className="text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600">
+            View Work
+            <span className="group-hover:rotate-90 duration-300">
+              <HiArrowNarrowRight className="ml-3" />
+            </span>
           </button>
         </div>
       </div>
